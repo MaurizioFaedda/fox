15c3cfe4996188a41cb1d8752e1f86b9ee7a2f29
Pietro <marrazzo.pietroalfonso@gmail.com>
2021-04-20 14:55:19 +0200

    titolo app

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Text.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Try
-        </a>
+        <h2>Gestione Aziendale</h2>
       </header>
     </div>
   );
