75d0e0f5f001fb0868e1e765a75f110c26f717b3
Pietro <marrazzo.pietroalfonso@gmail.com>
2021-04-20 16:40:35 +0200

    material-commit

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Theme, makeStyles, createStyles } from "@material-ui/core/styles";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Gestione Aziendale, prova sono un test</h2>
+        <h2>Gestione Aziendale</h2>
+
+        {/* button 1 - show companies */}
+        {/* <Button /> */}
+
+        {/* button 2 - show users */}
+        {/* <Button /> */}
       </header>
     </div>
   );
