343d3ea6b9c4630d253367a0f2dc6c15891a464f
Maurizio <maurizio.faedda1@gmail.com>
2021-04-21 12:23:10 +0200

    bug fixed

--- src/components/companies/CompanyActionBtn.tsx
+++ src/components/companies/CompanyActionBtn.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import AddIcon from "@material-ui/icons/Add";
@@ -6,35 +6,58 @@ import EditIcon from "@material-ui/icons/Edit";
 
 type IProps = {
   typeIcon: any;
-  isActived: Boolean;
+  isActived: boolean;
+  onClickEvent: Function;
+  buttonType?: string;
 };
 const CompanyActionBtn = (props: IProps) => {
-  const { typeIcon, isActived } = props;
+  const { typeIcon, isActived, buttonType, onClickEvent } = props;
+  const [title, setTitle] = useState<string>("");
+  useEffect(() => {
+    switch (typeIcon) {
+      case "Delete":
+        setTitle("Delete");
+        break;
+      case "Add":
+        setTitle("Add");
+
+        break;
+      case "Edit":
+        setTitle("Edit");
+
+        break;
+      default:
+        break;
+    }
+  }, []);
   return (
-    <>
-      {(() => {
-        switch (typeIcon) {
-          case "Add":
-            return (
-              <IconButton>
-                <AddIcon />
-              </IconButton>
-            );
-          case "Edit":
-            return (
-              <IconButton>
-                <EditIcon />
-              </IconButton>
-            );
-          case "Delete":
-            return (
-              <IconButton disabled={{}}>
-                <DeleteIcon />
-              </IconButton>
-            );
-        }
-      })()}
-    </>
+    <button disabled={isActived} onClick={() => onClickEvent}>
+      {title}
+    </button>
+    // <>
+    //   {(() => {
+    //     switch (typeIcon) {
+    //       case "Add":
+    //         return (
+    //           <IconButton>
+    //             <AddIcon />
+    //           </IconButton>
+    //         );
+    //       case "Edit":
+    //         return (
+    //           <IconButton disabled={isActived}>
+    //             <EditIcon />
+    //           </IconButton>
+    //         );
+    //       case "Delete":
+    //         return (
+    //           <IconButton disabled={isActived}>
+    //             <DeleteIcon />
+    //           </IconButton>
+    //         );
+    //     }
+    //   })()}
+    // </>
   );
 };
 
