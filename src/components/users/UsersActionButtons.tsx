51a844e31d6d3108321e3a00fe6154c150011165
Pietro <marrazzo.pietroalfonso@gmail.com>
2021-04-21 12:49:26 +0200

    users button added & fix company isActived

--- src/components/users/UsersActionButtons.tsx
+++ src/components/users/UsersActionButtons.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import AddIcon from "@material-ui/icons/Add";
@@ -6,35 +6,34 @@ import EditIcon from "@material-ui/icons/Edit";
 
 type IProps = {
   typeIcon: any;
+  disabled: boolean;
+  onClickEvent: Function;
 };
 
 const UsersActionBtn = (props: IProps) => {
-  const { typeIcon } = props;
+  const { typeIcon, disabled, onClickEvent } = props;
+  const [title, setTitle] = useState<string>("");
+  useEffect(() => {
+    switch (typeIcon) {
+      case "Delete":
+        setTitle("Delete");
+        break;
+      case "Add":
+        setTitle("Add");
+
+        break;
+      case "Edit":
+        setTitle("Edit");
+
+        break;
+      default:
+        break;
+    }
+  }, []);
   return (
-    <>
-      {(() => {
-        switch (typeIcon) {
-          case "Delete":
-            return (
-              <IconButton>
-                <DeleteIcon />
-              </IconButton>
-            );
-          case "Add":
-            return (
-              <IconButton>
-                <AddIcon />
-              </IconButton>
-            );
-          case "Edit":
-            return (
-              <IconButton>
-                <EditIcon />
-              </IconButton>
-            );
-        }
-      })()}
-    </>
+    <button disabled={disabled} onClick={() => onClickEvent}>
+      {title}
+    </button>
   );
 };
 
