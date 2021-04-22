0b1d5a2527a7a145517397cb8daa7d25412f8a63
Maurizio <maurizio.faedda1@gmail.com>
2021-04-22 17:03:54 +0200

    addForm

---
+++ src/components/companies/FormInput.tsx
@@ -0,0 +1,46 @@
+import { makeStyles, Theme } from "@material-ui/core";
+import { createStyles } from "@material-ui/styles";
+import React from "react";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    form: {
+      display: "flex",
+      flexDirection: "column",
+      position: "absolute",
+      top: "40%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      padding: 25,
+      backgroundColor: "#fff",
+    },
+    input: {
+      border: "1px solid black",
+    },
+  })
+);
+
+type IProps = {};
+
+const FormInput = (props: IProps) => {
+  const c = useStyles();
+  return (
+    <div>
+      <form className={c.form}>
+        <label>Name</label>
+        <input type="text" className={c.input} name="name" />
+        <label>Username</label>
+        <input type="text" className={c.input} name="username" />
+        <label>Id</label>
+        <input type="text" className={c.input} name="id" />
+        <label>Age</label>
+        <input type="text" className={c.input} name="age" />
+        <label>Birthday</label>
+        <input type="text" className={c.input} name="birthday" />
+        <button>Save datas</button>
+      </form>
+    </div>
+  );
+};
+
+export default FormInput;
