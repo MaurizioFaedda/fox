a3e50ba57712fbc8b8d9c58b0e9c461f13b64016
Maurizio <maurizio.faedda1@gmail.com>
2021-04-22 11:05:29 +0200

    DeleteFunction

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Theme, makeStyles, createStyles } from "@material-ui/core/styles";
 import CompaniesList from "./companies/CompaniesList";
 import UsersList from "./users/UsersList";
 import CompanyBtn from "../components/companies/CompanyBtn";
 import UsersBtn from "../components/users/actionButtons/UsersBtn";
+import { ICompanies } from "./companies/type";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -23,7 +24,19 @@ const Home = (props: IProps) => {
   const [showTable, setShowTable] = useState<Boolean>(false);
   const [showTableUsers, setShowTableUsers] = useState<Boolean>(false);
   const [selected, setSelected] = React.useState<number>();
+  const [companiesList, setCompaniesList] = useState<ICompanies[]>([]);
+
   const c = useStyles();
+
+  useEffect(() => {
+    fetch(
+      `https://my-json-server.typicode.com/MaurizioFaedda/companies-json/db`
+    )
+      .then((response) => response.json())
+      .then((json) => setCompaniesList(json["companies"]));
+
+    // };
+  }, []);
 
   const handleToggle = () => {
     if (showTable) {
@@ -49,6 +62,16 @@ const Home = (props: IProps) => {
     setSelected(index);
   };
 
+  const deleteSelected = (selected: number) => {
+    console.log(selected);
+    const newArr = companiesList.filter((item) => {
+      return companiesList.indexOf(item) + 1 !== selected;
+    });
+    setCompaniesList(newArr);
+    console.log(selected);
+    console.log(companiesList);
+  };
+
   return (
     <div>
       <header className={c.header}>
@@ -60,6 +83,8 @@ const Home = (props: IProps) => {
         show={showTable}
         selected={selected}
         handleFocusOnClick={handleFocusOnClick}
+        list={companiesList}
+        deleteSelected={deleteSelected}
       />
 
       {/* button users list */}
