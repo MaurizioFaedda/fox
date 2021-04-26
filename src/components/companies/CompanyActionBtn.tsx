f39c733e0ebba9ee178a5adca083c8e3a18e3aca
Maurizio <maurizio.faedda1@gmail.com>
2021-04-26 12:01:45 +0200

    input search added

--- src/components/companies/CompanyActionBtn.tsx
+++ src/components/companies/CompanyActionBtn.tsx
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+      },
+    },
+  })
+);
 
 type IProps = {
   typeIcon: any;
   disabled: boolean;
   onClickEvent: Function;
   selected?: number;
+  color: any;
 };
 const CompanyActionBtn = (props: IProps) => {
-  const { typeIcon, disabled, onClickEvent, selected } = props;
+  const { typeIcon, disabled, onClickEvent, selected, color } = props;
   const [title, setTitle] = useState<string>("");
+
+  const c = useStyles();
+
   useEffect(() => {
     switch (typeIcon) {
       case "Delete":
@@ -27,12 +43,22 @@ const CompanyActionBtn = (props: IProps) => {
     }
   }, []);
   return (
-    <button
-      disabled={selected ? false : disabled}
-      onClick={() => onClickEvent()}
-    >
-      {title}
-    </button>
+    // <button
+    //   disabled={selected ? false : disabled}
+    //   onClick={() => onClickEvent()}
+    // >
+    //   {title}
+    // </button>
+    <div className={c.root}>
+      <Button
+        onClick={() => onClickEvent()}
+        disabled={selected ? false : disabled}
+        variant="contained"
+        color={color}
+      >
+        {title}
+      </Button>
+    </div>
   );
 };
 
